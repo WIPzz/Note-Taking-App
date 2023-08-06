e1c2b3aa89eec2cefb5ec46e5f1e36f9be09c6ae
WIPzz <139990034+WIPzz@users.noreply.github.com>
2023-08-06 18:55:14 +0200

    Add files via upload

---
+++ Note-Taking App/Note-Taking App/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Note_Taking_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 76);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(330, 348);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(458, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add Note";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.addNoteBttn);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(12, 26);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(330, 29);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(628, 380);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 35);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Delete Note";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.delNoteBttn);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(458, 421);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(160, 35);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Edit Note";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.editNoteBttn);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(458, 26);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(330, 348);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Title";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Note";
+            this.columnHeader2.Width = 258;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Location = new System.Drawing.Point(12, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 13);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "Title:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Location = new System.Drawing.Point(12, 61);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 13);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.Text = "Text:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 489);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Note-Taking App/Note-Taking App/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+/*
+x -Concept
+    Left side
+        Title
+        Note field
+    Right Side
+        List with all notes
+        Delete Note
+        Add Note
+        Edit Note
+x - Get the text of textBox1 & 2
+x - Add Note button saves the text of textBox1 & 2 into the list
+x - Clear text if you add a note
+x - Able to select a note on the list
+x - In the list:
+    Show the title of the note on the left side
+    Show the note itself on the right side
+x - Selected note can be deleted
+x - Selected note can be edited
+    edit mode is on the left side
+
+
+
+Fix:
+x - Error if delete button is pressed and no row is selected
+x - If editing a note, change the button text to "update", update the old entry with the new one
+x - Dont accept empty entries
+x - App crashes if pressing the edit button without selecting a row
+
+
+
+
+*/
+
+
+
+namespace Note_Taking_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Add button
+        private void addNoteBttn(object sender, EventArgs e)
+        {
+            // Check for empty or null in textBox1 & 2
+            if (!string.IsNullOrEmpty(textBox1.Text) && !string.IsNullOrEmpty(textBox2.Text))
+            {
+                string title = textBox2.Text;
+                string text = textBox1.Text;
+
+                ListViewItem list = new ListViewItem(title);
+                list.SubItems.Add(text);
+                listView1.Items.Add(list);
+
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+        }
+
+        // Delete button
+        private void delNoteBttn(object sender, EventArgs e)
+        {
+            //Check if an entry is selected
+            if (listView1.SelectedItems.Count == 0) return;
+            
+            listView1.Items.Remove(listView1.SelectedItems[0]);
+        }
+
+        // Edit button
+        private bool editMode = false;
+        private void editNoteBttn(object sender, EventArgs e)
+        {
+            //Check if an entry is selected
+            if (listView1.SelectedItems.Count == 0) return;
+            
+            if (editMode)
+            {
+                // Update the selected item and subitems
+                ListViewItem selectedItem = listView1.SelectedItems[0];
+                selectedItem.SubItems[0].Text = textBox2.Text; 
+                selectedItem.SubItems[1].Text = textBox1.Text; 
+
+                button3.Text = "Edit Note";
+                editMode = false;
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+            else
+            {
+                button3.Text = "Update";
+                editMode = true;
+
+                // Show the subitems in the edit box
+                string title = listView1.SelectedItems[0].SubItems[0].Text;
+                string text = listView1.SelectedItems[0].SubItems[1].Text;
+
+                textBox1.Text = text;
+                textBox2.Text = title;
+            }
+        }
+
+    }
+}
